ec60babdc8db277e5d4a86bcd9def454e81fd2e2
Manh Linh <mlinhalone@gmail.com>
2024-11-21 18:47:50 +0700

    update

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,4 +1,30 @@
 function ProductList() {
-  return <div>Product List</div>;
+  return (
+    <div className="container">
+      <h1 className="text-center my-2">Product List</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row"></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td scope="row"></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default ProductList;
